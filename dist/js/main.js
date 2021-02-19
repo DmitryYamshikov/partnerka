28ea1a030122417d59acba3a2e3f655a41fb1959
Dmitry <DmitryYamshikov@gmail.com>
2021-02-20 01:08:37 +0700

    zayavki-page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", () => {
+    //открытие формы воостановления пароля
     const enterForms = document.querySelectorAll('.enter .form');
     const recoverPass = document.querySelector('a[recover-pass]');
 
@@ -10,7 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
             openForm('form_pass-recovery');
         });
     }
-
+    // форма регистрации
     const registerLink = document.querySelector('.link-registr a');
     if (registerLink) {
         registerLink.addEventListener('click', function (e) {
@@ -18,10 +19,12 @@ window.addEventListener("DOMContentLoaded", () => {
             openForm('form_registration');
         });
     }
+    //открытие формы
     function openForm(formClass) {
-        enterForms.forEach(item => item.classList.remove('active'));
+        if (enterForms) { enterForms.forEach(item => item.classList.remove('active')); }
         document.querySelector(`.${formClass}`).classList.add('active');
     }
+    //форма входа
     const loginForm = document.querySelector('.form_login');
     if (loginForm) {
         loginForm.addEventListener('submit', (e) => {
@@ -29,18 +32,18 @@ window.addEventListener("DOMContentLoaded", () => {
             location.href = '/zayavki.html';
         });
     }
-
+    // регулирование ширину инпутов
     const formInput = document.querySelectorAll('.form-profile input');
     if (formInput) {
         formInput.forEach(item => {
             setInputSize(item);
-            item.addEventListener('change', ()=>{
+            item.addEventListener('change', () => {
                 const btn = document.querySelector('.form-profile .btn');
                 console.log(btn);
                 btn.removeAttribute('disabled');
             });
         });
-        
+
     }
 
     function setInputSize(input) {
@@ -49,16 +52,39 @@ window.addEventListener("DOMContentLoaded", () => {
         input.style.width = `${x * 11}px`;
     }
 
+    //табы в анкете
     const tabHeaderItems = document.querySelectorAll('.form-profile__tabs-header span');
 
     if (tabHeaderItems) {
-        tabHeaderItems.forEach((item,i)=>{
+        tabHeaderItems.forEach((item, i) => {
             item.addEventListener('click', () => {
                 const tabItems = document.querySelectorAll('.form-profile__tabs-item');
                 tabItems.forEach(item => item.classList.remove('active'));
                 tabHeaderItems.forEach(item => item.classList.remove('active'));
                 item.classList.add('active');
                 tabItems[i].classList.add('active');
+            });
+        });
+    }
+    //вызово формы для добавления сотрудника
+    const btnCallModal = document.querySelector('[call-modal]');
+
+    if (btnCallModal) {
+        btnCallModal.addEventListener('click', ()=>{
+            openForm('modal_add-person');
+        });
+    }
+    //открытие доп информации в заявках
+    const aplicTableRow = document.querySelectorAll('.aplication-table .table__row');
+
+    if (aplicTableRow) {
+        aplicTableRow.forEach(item=> {
+            item.addEventListener('click', function(e) {
+                e.stopPropagation();
+                if (e.target.closest('[data="info"]')) {
+                    this.classList.toggle('active');
+                    this.nextSibling.classList.toggle('active');
+                }
             });
         });
     }

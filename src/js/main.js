cea93e684401d3761401334f2c509e01dbbbe036
Dmitry <DmitryYamshikov@gmail.com>
2021-02-23 01:03:43 +0700

    adaptive menu

--- src/js/main.js
+++ src/js/main.js
@@ -92,4 +92,13 @@ window.addEventListener("DOMContentLoaded", () => {
             });
         });
     }
+
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.menu');
+    if (burger) {
+        burger.addEventListener('click', ()=>{
+            burger.classList.toggle('active');
+            menu.classList.toggle('active');
+        });
+    }
 });
